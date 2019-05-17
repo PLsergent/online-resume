c8b178fa4f64874a5a885ccf38e6575756f282dd
PLsergent <papa.louis59@gmail.com>
2019-05-17 16:40:41 +0200

    add comments

--- app/online_resume_app.py
+++ app/online_resume_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# sudo docker build --tag=online-resume .
 # sudo docker run --name online-resume -d -p 80:5000 --rm online-resume:latest
 
 
